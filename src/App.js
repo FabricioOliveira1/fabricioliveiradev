688e9f19913d1ea56e5398b470c0a8097746fb6d
Fabricio Oliveira <fabricioliveira021@gmail.com>
2024-08-21 23:47:56 -0300

    Criado componentes de sessao sobre e Contatos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import About from './components/About';
 import Header from './components/Header';
 import Main from './components/Main';
 
+import './App.css'
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <About />
+      <Contact />
     </div>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,7 +7,7 @@ const Main = () => {
     <section className='main'>
       <h1>Olá eu sou<br/> <strong>Fabricio oliveira</strong> Desenvolvedor<br/> Front-end</h1>
       <p>Web Developer</p>
-      <CTABUtton /> 
+      <CTABUtton text={'Veja Meus Projetos'}/> 
       <img src={astronauta} alt='Astronauta no espaço'/>
     </section>
   )
