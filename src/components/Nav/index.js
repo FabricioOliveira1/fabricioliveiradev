73305187da986005b8d4b627d66a85e68f9beee1
Fabricio Oliveira <fabricioliveira021@gmail.com>
2024-10-04 13:35:07 -0300

    Feito parte responsiva do header, e foi ajustado nome do site

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,3 +1,4 @@
+import './Nav.css'
 import MenuButton from '../ui/Buttons/MenuButton';
 
 const Nav = ({ handleMenuOpen }) => {
@@ -5,6 +6,14 @@ const Nav = ({ handleMenuOpen }) => {
 
   return (
     <nav className='nav'>
+      <div className='navLinks'>
+        <ul className='navLinks-list'>
+          <li className='navLinks-listItem'><a href='#home'>Inicio</a></li>
+          <li className='navLinks-listItem'><a href='#about'>Sobre</a></li>
+          <li className='navLinks-listItem'><a href='#portifolio'>Repositórios</a></li>
+          <li className='navLinks-listItem'><a href='#projetos'>Projetos</a></li>
+        </ul>
+      </div>
       <MenuButton  handleMenuOpen={handleMenuOpen}/>
     </nav>
   )
